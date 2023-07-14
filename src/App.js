e74cd5da1a7f49ab237e9c9fc744d29f272c28fe
shivamtripathi19 <shivam.tripathi@tokopedia.com>
2023-07-14 11:35:03 +0530

    feat(POC): Push notifications with Firebase Cloud Messaging

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+import { messaging } from "./firebase";
+import { getToken } from "firebase/messaging";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  //It takes permission from user whether website can send notifications.
+  async function requestPermission() {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      // Generate Token
+      const token = await getToken(messaging, {
+        vapidKey:''
+      });
+      console.log("Token Gen", token);
+      // Send this token  to server ( db)
+    } else if (permission === "denied") {
+      alert("You denied for the notification");
+    }
+  }
+
+  useEffect(() => {
+    // Req user for notification permission
+    requestPermission();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
